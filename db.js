4332ad973baf2d476950c971717a24e51be154d8
phongpanphokaew <s6206021631029@emailkmutnb.ac.th>
2024-03-02 23:56:45 +0700

    update db

--- db.js
+++ db.js
@@ -7,6 +7,6 @@ require('dotenv').config()
 //     logging: console.log,
 // });
 // console.log(process.env.DB_USERNAME);
-const dbUrl = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@node59682-js-project.proen.app.ruk-com.cloud:11942/Gallery`;
+const dbUrl = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@node59682-js-project.proen.app.ruk-com.cloud/Gallery`;
 const sequelize = new Sequelize(dbUrl);
 module.exports = { Sequelize, sequelize };
