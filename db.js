0f4efeb14575f6809cfa63058fe264e77e27ea40
phongpanphokaew <s6206021631029@emailkmutnb.ac.th>
2024-05-22 23:33:14 +0700

    up

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize');
 require('dotenv').config()
-// const sequelize = new Sequelize('database', 'username', 'password', {
-//     host: 'localhost',
-//     dialect: 'sqlite',
-//     storage: './Database/SQGallerysqlite',
-//     logging: console.log,
-// });
-// console.log(process.env.DB_USERNAME);
-const dbUrl = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@node59700-back-end.proen.app.ruk-com.cloud/Gallery`;
+ //const sequelize = new Sequelize('database', 'username', 'password', {
+    // host: 'localhost',
+    // dialect: 'sqlite',
+   //  storage: './Database/SQGallerysqlite',
+   //  logging: console.log,
+ //});
+ console.log(process.env.DB_USERNAME);
+const dbUrl = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@node62198-project.proen.app.ruk-com.cloud/`;
 const sequelize = new Sequelize(dbUrl);
 module.exports = { Sequelize, sequelize };
